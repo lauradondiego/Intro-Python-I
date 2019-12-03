dd4e62b162dcce21c8d5e9727bb9e34803efcfea
laura <lauradondiego@gmail.com>
2019-12-03 17:34:34 -0500

    compelted dictionaries and comprehensions

--- src/11_args.py
+++ src/11_args.py
@@ -6,12 +6,22 @@
 
 # YOUR CODE HERE
 
+
+def f1(num1, num2):
+    return num1 + num2
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*args):
+    return sum(args)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
