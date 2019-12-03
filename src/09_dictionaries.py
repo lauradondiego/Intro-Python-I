6230981597517012224746e8d64054c3d46e9171
laura <lauradondiego@gmail.com>
2019-12-03 16:51:23 -0500

    completed functions

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,10 +35,12 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints["lat"] = 21
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+waypoints["a place"]=
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
